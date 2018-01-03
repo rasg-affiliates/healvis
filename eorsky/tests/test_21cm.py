6f184a01dacddc191377c65cab114065ba54a48e
Adam Lanman <adam_lanman@brown.edu>
2018-01-03 11:19:31 -0500

    Update test scripts

--- eorsky/tests/test_21cm.py
+++ eorsky/tests/test_21cm.py
@@ -13,9 +13,14 @@ N = 256    #Pixels
 
 dat = np.fromfile(dat_file,dtype=np.float32).reshape((N,N,N))
 dat -= np.mean(dat)
-ks, pk = r_pspec_1D(dat,L)
+ks, pk = r_pspec_1D(dat,L, cosmo=True)
 pk *= (ks**3/2*np.pi**2)
 
+
+Nz = N
+dz = L/float(N)
+kfact = 2*np.pi
+kz = np.fft.fftfreq(Nz,d=dz)*kfact   #Mpc^-1
 
 ref_spectrum = np.genfromtxt(ref_file)
 ref_ks = ref_spectrum[:,0]
@@ -34,16 +39,18 @@ ek.N = N
 #freqs = np.linspace(100,200,1024)[481:633]
 #freqs = np.linspace(100,200,204)[95:125]
 freqs = np.linspace(182.95-30.72/2.,182.95+30.72/2., 384)    ## Actual MWA channel structure... I've been doing it wrong.
+freqs = freqs[100:200]
+print "Bandwidth: ", freqs[-1] - freqs[0]
 ek.freqs = freqs
 ek.nside=512
 
 ek.update()
 ek.slice()
 
-s_ks, s_pk, errs = r_pspec_sphere(ek.hpx_shell, ek.nside, 15, freqs=ek.freqs)
+s_ks, s_pk, errs = r_pspec_sphere(ek.hpx_shell, ek.nside, 20, freqs=ek.freqs)
 s_pk *= s_ks**3/(2*np.pi**2)
 
-p.plot(ks, pk/100.,marker='.',label='Meas')
+p.plot(ks, pk,marker='.',label='Meas')
 p.plot(ref_ks,ref_pk,marker='.',label="Ref")
 p.plot(s_ks,s_pk,marker='.',label="spherical")
 p.yscale('log'); p.xscale('log')
--- eorsky/tests/test_rectloop.py
+++ eorsky/tests/test_rectloop.py
@@ -6,22 +6,21 @@ import pylab
 ek = eorsky()
 L = 500
 N = 256
-NSIDE=1024
+NSIDE=512
 
 #freqs=np.linspace(100,200,1024)
 #freqs = freqs[430:633]
-freqs=np.linspace(100,200,203)
-freqs = freqs[85:115]
-##Select 20 MHz
+#freqs=np.linspace(100,200,203)
+#freqs = freqs[85:115]
+freqs = np.linspace(182.95-30.72/2.,182.95+30.72/2., 384)    ## Actual MWA channel structure... I've been doing it wrong.
 ek.nside = NSIDE
-#freqs
 ek.freqs = freqs
 
 ek.make_gaussian_cube(N,L,var=1.0)
 ek.update()
 k0,pk0 = r_pspec_1D(ek.rect_cube,L)   #The pspec of the input gaussian cube
 ek.slice()
-
+ek.update()
 
 #k1, pk1,err1 = r_pspec_sphere(ek.hpx_shell, ek.nside, 15, freqs=ek.freqs,N_sections=5,Nkbins=100,pyramid=True)
 k1, pk1,err1 = r_pspec_sphere(ek.hpx_shell, ek.nside, 15, freqs=ek.freqs,N_sections=5,Nkbins=100,lomb_scargle=True)
