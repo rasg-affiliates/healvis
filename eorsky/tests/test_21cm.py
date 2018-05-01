86e7b58a46af3dbbde42cd8f755d8308ae87d609
aelanman <adam_lanman@brown.edu>
2018-05-01 17:10:50 -0400

    Updates to tests

--- eorsky/tests/test_21cm.py
+++ eorsky/tests/test_21cm.py
@@ -12,14 +12,16 @@ N = 256    #Pixels
 
 dat = np.fromfile(dat_file,dtype=np.float32).reshape((N,N,N))
 dat -= np.mean(dat)
-ks, pk = r_pspec_1D(dat,L, cosmo=True)
+
+r_mpc = np.linspace(0,300,N)    # Test the DFT mode.
+ks, pk = r_pspec_1D(dat,L, r_mpc = r_mpc, cosmo=True)
 pk *= (ks**3/2*np.pi**2)
 
 
-Nz = N
-dz = L/float(N)
-kfact = 2*np.pi
-kz = np.fft.fftfreq(Nz,d=dz)*kfact   #Mpc^-1
+#Nz = N
+#dz = L/float(N)
+#kfact = 2*np.pi
+#kz = np.fft.fftfreq(Nz,d=dz)*kfact   #Mpc^-1
 
 ref_spectrum = np.genfromtxt(ref_file)
 ref_ks = ref_spectrum[:,0]
