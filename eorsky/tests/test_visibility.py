Fix visibility units

eorsky/tests/test_visibility.py
@@ -70,11 +70,9 @@ def test_vis_calc():
     centers = [center]
     npix = nside**2 * 12
     shell = np.zeros((npix, nfreqs))
-#    ind = hp.ang2pix(nside, centers[0][0], centers[0][1], lonlat=True)
-    shell[ind] = 1
-    pix_area = 4*np.pi/float(npix)
-    shell[ind] /= pix_area  # K
-#    import IPython; IPython.embed()
+    pix_area = 4* np.pi / float(npix)
+    shell[ind] = 1  # Jy/pix
+    shell[ind] /= visibility.jy2Tstr(freqs[0], pix_area)  # K
 
     obs = visibility.observatory(latitude, longitude, array=[bl], freqs=freqs)
     obs.pointing_centers = centers
@@ -110,8 +108,8 @@ def test_offzenith_vis():
     # Choose an index 5 degrees off from the pointing center
     phi, theta = hp.pix2ang(Nside, ind, lonlat=True)
     ind = hp.ang2pix(Nside, phi, theta-5, lonlat=True)
-    shell[ind] = 1  # K * sr
-    shell[ind] /= pix_area  # K
+    shell[ind] = 1  # Jy/pix
+    shell[ind] /= visibility.jy2Tstr(freqs[0], pix_area)  # K
 
     obs = visibility.observatory(latitude, longitude, array=[bl], freqs=freqs)
     obs.pointing_centers = [[phi, theta]]
@@ -140,13 +138,3 @@ if __name__ == '__main__':
     test_offzenith_vis()
     #test_vis_calc()
     #test_az_za()
-
-
-
-# scripts:
-#   Script to calculate visibilities from a gaussian sky given gaussian beams of different widths. --- Confirm the relationship between beam width and covariance matrices
-#   !Covariance per freq wrt an ensemble of gaussian skies with 24 hours -- (more demanding; may need mpi)
-#   Get overlap between fields of view and primary beams for different centers --- how does that relate with correlation?
-#   Look at effect of resolution and time cadence, with varying beam width
-#   Covariance binning of random visibilities
-

eorsky/visibility.py
@@ -8,9 +8,54 @@ from astropy.constants import c
 from astropy.time import Time
 from astropy.coordinates import Angle, AltAz, EarthLocation, ICRS
 import healpy as hp
+from numba import jit
+
+from pyuvdata import UVBeam
 
 c_ms = c.to('m/s').value
 
+def jy2Tstr(f, bm = 1.0):
+    '''Return [K sr] / [Jy] vs. frequency (in Hz)
+        Arguments:
+            f = frequencies (Hz)
+            bm = Reference area (defaults to 1 steradian)
+    '''
+    c_cmps = c_ms * 100.   # cm/s
+    k_boltz = 1.380658e-16   # erg/K
+    lam = c_cmps / f   #cm
+    return 1e-23 * lam**2 / (2 * k_boltz * bm)
+
+class powerbeam(UVBeam):
+    """
+    Interface for using beamfits files here.
+    """
+
+    ### TODO Develop and test!
+    def __init__(self, beamfits_path=None):
+        super(powerbeam, self).__init__()
+        if beamfits_path is not None:
+            self.read_beamfits(beamfits_path)
+
+    def read_beamfits(self, beamfits_path):
+        super(powerbeam, self).read_beamfits(beamfits_path)
+        if not self.beam_type == 'power':
+            self.efield_to_power()
+        self.interpolation_function = 'az_za_simple'
+
+    def beam_val(self, az, za, freq_Hz=None):
+        """
+        az, za = radians
+        """
+        az = np.array(az)
+        za = np.array(za)
+        if isinstance(az, float):
+            az = np.array([az])
+        if isinstance(za, float):
+            za = np.array([za])
+        if freq_Hz is None:
+            freq_Hz = self.freq_array[0,0]
+        interp_beam, interp_basis = self.interp(az_array = az, za_array = za, freq_array = np.array([freq_Hz]), reuse_spline=True)
+        return np.squeeze(np.sum(interp_beam, axis=2))  #  Sum over feeds (XX + XY + YX + YY)
 
 class analyticbeam(object):
 
@@ -41,6 +86,19 @@ class analyticbeam(object):
         if self.beam_type == 'gaussian':
             return np.exp(-(za**2) / (2 * self.sigma**2))  # Peak normalized
 
+@jit
+def make_fringe(az, za, freq, enu):
+    """
+    az, za = Azimuth, zenith angle, radians
+    freq = frequeny in Hz
+    """
+    pos_l = np.sin(az) * np.sin(za)
+    pos_m = np.cos(az) * np.sin(za)
+    pos_n = np.cos(za)
+    lmn = np.array([pos_l, pos_m, pos_n])
+    uvw = np.outer(enu, 1/(c_ms / freq))  # In wavelengths
+    udotl = np.einsum("jk,jl->kl", lmn, uvw)
+    return np.exp(2j * np.pi * udotl)
 
 class baseline(object):
 
@@ -55,20 +113,8 @@ class baseline(object):
         if degrees:
             az *= np.pi/180.
             za *= np.pi/180.
-        pos_l = np.sin(az) * np.sin(za)
-        pos_m = np.cos(az) * np.sin(za)
-        pos_n = np.cos(za)
-        lmn = np.array([pos_l, pos_m, pos_n])
-        uvw = np.outer(self.enu, 1/(c_ms / freq_Hz.astype(float)))  # In wavelengths
-        #udotl = np.einsum("ijk,il->jkl", lmn, uvw)
-        udotl = np.einsum("jk,jl->kl", lmn, uvw)
-        return np.exp(2j * np.pi * udotl)
-        #pos_l = np.sin(az) * np.sin(za)
-        #pos_m = np.cos(az) * np.sin(za)
-        #pos_n = np.cos(za)
-        #self.lmn = np.array([pos_l, pos_m, pos_n])
-        #uvw = self.enu / (c_ms / float(freq_Hz))
-        #return np.exp(2j * np.pi * (pos_l * uvw[0] + pos_m * uvw[1] + pos_n * uvw[2]))
+        freq_Hz = freq_Hz.astype(float)
+        return make_fringe(az, za, freq_Hz, self.enu)
 
     def plot_fringe(self, az, za, freq=None, degrees=False, pix=None, Nside=None):
         import pylab as pl
@@ -132,11 +178,12 @@ class observatory:
             centers.append([zen_radec.ra.deg, zen_radec.dec.deg])
         self.pointing_centers = centers
 
-    def calc_azza(self, Nside, center):
+    def calc_azza(self, Nside, center, return_inds=False):
         """
         Set the az/za arrays.
             Center = lon/lat in degrees
             radius = selection radius in degrees
+            return_inds = Return the healpix indices too
         """
         if self.fov is None:
             raise AttributeError("Need to set a field of view in degrees")
@@ -154,6 +201,8 @@ class observatory:
         sdoty = np.array([np.dot(s, yvec) for s in vecs])
         za_arr = np.arccos(sdotz)
         az_arr = (np.arctan2(sdotx, sdoty) + np.pi) % (2 * np.pi)  # xy plane is tangent. Increasing azimuthal angle eastward, zero at North (y axis)
+        if return_inds:
+            return za_arr, az_arr, pix
         return za_arr, az_arr
 
     def set_fov(self, fov):
@@ -193,6 +242,9 @@ class observatory:
         Orthoslant project sections of the shell (fov=radius, looping over centers)
         Make beam cube and fringe cube, multiply and sum.
         shell (Npix, Nfreq) = healpix shell
+
+        Takes a shell in Kelvin
+        Returns visibility in Jy
         """
         if len(shell.shape) == 3:
             Nskies, Npix, Nfreqs = shell.shape
@@ -201,9 +253,11 @@ class observatory:
 
         assert Nfreqs == self.Nfreqs
         Nside = hp.npix2nside(Npix)
-        pixel_area_sr = 4*np.pi/float(Npix)
         bl = self.array[0]
+        pix_area_sr = 4*np.pi/float(Npix)
         freqs = self.freqs
+        conv_fact = jy2Tstr(np.array(freqs), bm = pix_area_sr)
+        print('conv_factor: ', conv_fact)
         visibilities = []
         for c in self.pointing_centers:
             za_arr, az_arr = self.calc_azza(Nside, c)
@@ -217,8 +271,8 @@ class observatory:
             radius = self.fov * np.pi / 180. * 1 / 2.
             cvec = hp.ang2vec(c[0], c[1], lonlat=True)
             pix = hp.query_disc(Nside, cvec, radius)
-            ogrid = shell[..., pix, :] * pixel_area_sr
+            ogrid = shell[..., pix, :]
             ## The last two axes are(pixels, freqs).
             # Optionally --- First axis = ensemble
             visibilities.append(np.sum(ogrid * beam_cube * fringe_cube, axis=-2))
-        return np.array(visibilities)
+        return np.array(visibilities, dtype=np.complex128)/conv_fact

scripts/vis_calc.py
@@ -1,3 +1,10 @@
+#!/bin/env python
+
+#SBATCH -J eorsky
+#SBATCH -t 8:00:00
+#SBATCH -n 16
+#SBATCH --mem=20G
+
 
 """
 Calculate visibilities for:
@@ -12,10 +19,11 @@ import numpy as np
 from eorsky import visibility
 import pylab as pl
 from scipy.stats import binned_statistic
-import os, sys
+import os, sys, yaml
 from pyuvsim.simsetup import check_file_exists_and_increment
 from pyuvdata import UVData
 from pyuvdata import utils as uvutils
+from eorsky import comoving_voxel_volume
 
 ofilename = 'eorsky_gauss_sim.uv'
 
@@ -28,7 +36,7 @@ except KeyError:
 latitude  = -30.7215277777
 longitude =  21.4283055554
 altitude = 1073.
-fov = 10  #Deg
+fov = 100  #Deg
 ant1_enu = np.array([0, 0, 0])
 ant2_enu = np.array([0.0, 14.6, 0])
 bl = visibility.baseline(ant1_enu, ant2_enu)
@@ -37,21 +45,30 @@ bl = visibility.baseline(ant1_enu, ant2_enu)
 
 t0 = 2451545.0      #Start at J2000 epoch
 #Ntimes = 7854  # 24 hours in 11 sec chunks
-Nfreqs = 100
-Ntimes = 2000
+Nfreqs = 384
+Ntimes = 5000
 #Ntimes = 500
 time_arr = np.linspace(t0, t0 + Ntimes/float(3600. * 24 / 11.), Ntimes)
 
 # Frequency
-freqs  = np.linspace(1e8, 1.5e8, Nfreqs)  #Hz
+freqs  = np.linspace(1e8, 1.3e8, Nfreqs)  #30 MHz
 Nfreqs = freqs.size
 
 # Shells
-Nside = 64
+Nside = 128
 Npix = 12*Nside**2
 sig = 3.0
 Nskies = 1
-shell0 = np.random.normal(0.0, sig, (Nskies, Npix, Nfreqs))
+shell0 = np.zeros((Nskies,Npix,Nfreqs), dtype=float)
+#shell0 = np.random.normal(0.0, sig, (Nskies, Npix, Nfreqs))
+dnu = np.diff(freqs)[0]/1e6
+om = 4*np.pi/float(Npix)
+Zs = 1420e6/freqs - 1
+dV0 = comoving_voxel_volume(Zs[Nfreqs/2], dnu, om)
+for fi in range(Nfreqs):
+    dV = comoving_voxel_volume(Zs[fi], dnu, om) 
+    s = sig  * np.sqrt(dV0/dV)
+    shell0[:,:,fi] = np.random.normal(0.0, s, (Nskies, Npix))
 
 #Make observatories
 visibs = []
@@ -69,39 +86,19 @@ for i,s in enumerate(sigmas):
     if task_id is not None:
         if not i == task_id:
             continue
-    print s
     sigs_used.append(s)
     obs.set_beam('gaussian', sigma=s)
     visibs.append(obs.make_visibilities(shell0))
-
-import IPython; IPython.embed()
-import sys; sys.exit()
+# Visibilities are in Jy
 
 uv = UVData()
 data_arr = visibs[0][:,0,:]  # (Nblts, Nskies, Nfreqs)
 data_arr = data_arr[:,np.newaxis,:,np.newaxis]  # (Nblts, Nspws, Nfreqs, Npol)
 
-# Convert from K str to Jy:
-
-def jy2Tstr(f):#, bm):
-    '''Return [mK sr] / [Jy] vs. frequency (in Hz)
-        f = frequencies (Hz!)
-    '''
-    c_cmps = 2.99792458e10   # cm/s
-    k_boltz = 1.380658e-16   # erg/K
-    lam = c_cmps / f   #cm
-    bm = 1.0 # steradian
-    return 1e-23 * lam**2 / (2 * k_boltz * bm) * 1e3
-
-conv_factor = jy2Tstr(freqs)
-
-data_arr[:,0,:,0] /= conv_factor
-
 # Get beam_sq_int
-
 za, az = obs.calc_azza(Nside, obs.pointing_centers[0])
 beam_sq_int = np.sum(obs.beam.beam_val(az, za)**2)
-beam_sq_int = np.ones(Nfreqs) * beam_sq_int
+beam_sq_int = np.ones(Nfreqs) * beam_sq_int * om 
 
 uv.Nbls = 1
 uv.Ntimes = Ntimes
@@ -115,12 +112,12 @@ uv.ant_2_array = np.ones(uv.Nblts, dtype=int)
 uv.baseline_array = uv.antnums_to_baseline(uv.ant_1_array, uv.ant_2_array)
 uv.time_array = time_arr
 uv.Npols = 1
-uv.polarization_array=np.array([-5])
+uv.polarization_array=np.array([1])
 uv.Nants_telescope = 2
 uv.Nants_data = 2
 uv.antenna_positions = uvutils.ECEF_from_ENU(np.stack([ant1_enu, ant2_enu]), latitude, longitude, altitude)
-uv.flag_array = np.zeros_like(uv.data_array).astype(bool)
-uv.nsample_array = np.ones_like(uv.data_array).astype(float)
+uv.flag_array = np.zeros(uv.data_array.shape).astype(bool)
+uv.nsample_array = np.ones(uv.data_array.shape).astype(float)
 uv.Nspws = 1
 uv.antenna_numbers = np.array([0,1])
 uv.antenna_names = ['ant0', 'ant1']