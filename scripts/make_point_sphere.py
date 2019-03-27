9e0b6dec0afdf91d2177b0491d91e1ddee944bf6
Adam Lanman <adam_lanman@brown.edu>
2019-03-27 15:38:06 -0400

    scripts for making special point source map and an imaging telescope layout

---
+++ scripts/make_point_sphere.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+
+import numpy as np
+import pylab as pl
+import healpy as hp
+
+from healvis.sky_model import SkyModel
+
+
+#-----------------------
+# Makes a healpix map with the following properties:
+#   - Nonzero pixels are equally-spaced.
+#   - Values are constant at each latitude
+#   - The value of each nonzero pixel is an integer indexing the latitude.
+#-----------------------
+
+nside0 = 12
+nside1 = 128
+freqs = np.array([100e6])
+
+dpix_spacing = (nside1/nside0) * np.degrees(hp.nside2resol(nside1))
+
+ra, dec = hp.pix2ang(nside0, np.arange(12*nside0**2), lonlat=True)
+udecs = np.unique(dec)
+map0 = np.zeros(12* nside0**2)
+for di,d in enumerate(udecs):
+    map0[dec==d] = di
+
+map1 = np.zeros(12*nside1**2)
+
+inds0 = hp.ang2pix(nside1, ra, dec, lonlat=True)
+
+map1[inds0] = map0
+map1 = map1[:,np.newaxis]
+
+sky = SkyModel(freqs=freqs, Nside=nside1, data=map1)
+
+sky.write_hdf5('healvis/data/imaging_test_map.hdf5', clobber=True)
