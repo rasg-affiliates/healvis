5fe73d40e8e90907e0c4fa0f7a29d1170b4a5279
Steven Murray <steven.g.murray@asu.edu>
2021-02-18 14:35:20 -0700

    maint: use setuptools_scm for versioning

--- setup.py
+++ setup.py
@@ -4,4 +4,20 @@
 
 from setuptools import setup
 
-setup()
+
+def branch_scheme(version):
+    """
+    Local version scheme that adds the branch name for absolute reproducibility.
+    If and when this is added to setuptools_scm this function and file can be removed.
+    """
+    if version.exact or version.node is None:
+        return version.format_choice("", "+d{time:{time_format}}", time_format="%Y%m%d")
+    else:
+        if version.branch == "main":
+            return version.format_choice("+{node}", "+{node}.dirty")
+        else:
+            return version.format_choice("+{node}.{branch}", "+{node}.{branch}.dirty")
+
+setup(
+    use_scm_version={"local_scheme": branch_scheme}
+)
